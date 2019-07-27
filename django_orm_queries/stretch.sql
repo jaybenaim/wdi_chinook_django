3641a30817237f2b8f9076aef6d883e7f0e26a6c
jaybenaim <benaimjacob@gmail.com>
2019-07-27 17:55:03 -0400

    stretch 2

--- django_orm_queries/stretch.sql
+++ django_orm_queries/stretch.sql
@@ -1,4 +1,23 @@
 ## STRETCH 
  
 ## 1. 
-SELECT title from chinook_album WHERE artist_id in (SELECT id FROM chinook_artist WHERE name='Queen');
+SELECT title from chinook_album WHERE artist_id in (SELECT id FROM chinook_artist WHERE name='Queen');                                                                                                                   
++-------------------+
+| title             |
+|-------------------|
+| Greatest Hits I   |
+| Greatest Hits II  |
+| News Of The World |
++-------------------+
+
+
+## 2 
+SELECT COUNT(name) FROM chinook_track where media_type_id in (SELECT id FROM chinook_mediatype where name='Protected MPEG-4 video file');                                                                                
++---------+
+| count   |
+|---------|
+| 214     |
++---------+
+
+# 3 
+
