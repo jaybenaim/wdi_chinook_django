76ca40bb21c3bfee89469620c1e6837c1ace3221
jaybenaim <benaimjacob@gmail.com>
2019-07-27 18:26:53 -0400

    complete stretch in stretch.sql

--- django_orm_queries/stretch.sql
+++ django_orm_queries/stretch.sql
@@ -20,4 +20,34 @@ SELECT COUNT(name) FROM chinook_track where media_type_id in (SELECT id FROM chi
 +---------+
 
 # 3 
+SELECT name from chinook_track where genre_id in (SELECT id from chinook_genre where name='Electronica/Dance') ORDER BY unit_price ASC LIMIT 1; 
++--------------------+
+| name               |
+|--------------------|
+| Instinto Colectivo |
++--------------------+
 
+# 4 
+SELECT NAME FROM chinook_artist WHERE name LIKE 'A%'; 
++---------------------------------------------------------------------------------------+
+| name                                                                                  |
+|---------------------------------------------------------------------------------------|
+| Aquaman                                                                               |
+| AC/DC                                                                                 |
+| Alanis Morissette                                                                     |
+| Audioslave                                                                            |
+SELECT 26 
+
+# 5 
+
+select * from chinook_playlist_tracks where playlist_id=1;
+
++------+---------------+------------+
+| id   | playlist_id   | track_id   |
+|------+---------------+------------|
+| 3285 | 1             | 3499       |
+| 3284 | 1             | 3498       |
+| 3283 | 1             | 3497       |
+| 3282 | 1             | 3496       |
+| 3281 | 1             | 3495       |
+SELECT 3285
