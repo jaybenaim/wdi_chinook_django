62d4bd1c292e89275d39c97e46bd5a2b1183c340
jaybenaim <benaimjacob@gmail.com>
2019-08-01 16:08:02 -0400

    part 4 complete

--- chinook/models.py
+++ chinook/models.py
@@ -15,6 +15,9 @@ class Album(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genres = models.ManyToManyField('Genre', through='Track')
+    media_types = models.ManyToManyField('MediaType', through='Track')
+
 
     def __str__(self): 
         return f'{self.title}'
@@ -23,14 +26,17 @@ class MediaType(models.Model):
     name = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    genres = models.ManyToManyField('Genre', through='Genre')
+    genres = models.ManyToManyField('Genre', through='Track')
+    albums = models.ManyToManyField(Album, through='Track')
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # media_types = models.ForeignKey('MediaType', on_delete=models.CASCADE)
-
+    media_types = models.ManyToManyField('MediaType', through='Track')
+    albums = models.ManyToManyField(Album, through='Track')
+    
     def __str__(self):
         return f'{self.name}'
 
